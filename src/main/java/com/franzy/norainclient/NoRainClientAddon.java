84f4dd405bdf9fcd38c3a0e862d14357190408bc
Franzy95 <fran.jemic95@gmail.com>
2025-09-10 21:24:39 +0200

    I fix it

--- src/main/java/com/franzy/norainclient/NoRainClientAddon.java
+++ src/main/java/com/franzy/norainclient/NoRainClientAddon.java
@@ -1,13 +1,31 @@
 package com.franzy.norainclient;
 
 import btw.BTWAddon;
+import btw.AddonHandler;
 
 public class NoRainClientAddon extends BTWAddon {
 
+    public NoRainClientAddon() {
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return "No Rain Addon";
+    }
+
+    @Override
+    public String getVersionString() {
+        return "1.0.1";
+    }
+
     @Override
     public void initialize() {
+        AddonHandler.logMessage(this.getName() + " Version " + this.getVersionString() + " Initializing...");
+
     }
 }
+
 
 
 
